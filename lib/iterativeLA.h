ad032ba2f9e7e4abb677101ef41b0cc3c16b52d0
nelsontodd <nelsonmorrow@utexas.edu>
2019-01-30 19:08:09 -0600

    Directory structure

---
+++ lib/iterativeLA.h
@@ -0,0 +1,13 @@
+#ifndef IterativeLA_Included
+#define IterativeLA_Included
+
+#include "matrix.h"
+
+enum state {SUCCESS, WONT_STOP, BAD_DIAGONAL, BAD_DATA};
+
+state jacobi(const Matrix& A, const Vector& b, Vector& x,
+	     int& maxIter, double tol);
+
+state gauss_seidel(const Matrix& A, const Vector& b, Vector& x,
+	     int& maxIter, double tol);
+#endif
